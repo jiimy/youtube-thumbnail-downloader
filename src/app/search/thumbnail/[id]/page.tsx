d2ca3b8a5d6202c0b062d8f07f4af5005f003b94
rudwnok <rudwnok123@naver.com>
2025-03-11 13:51:41 +0900

    수정

--- src/app/search/thumbnail/[id]/page.tsx
+++ src/app/search/thumbnail/[id]/page.tsx
@@ -1,14 +1,15 @@
 import ListSearch from '@/components/List/ListSearch';
 import ListThumbnail from '@/components/List/ListThumbnail';
-import React from 'react';
+import Loading from '@/components/loading/Loading';
+import React, { Suspense } from 'react';
 
 const page = ({ params }: { params: { id: string } }) => {
 
   return (
-    <div>
+    <Suspense fallback={<Loading />}>
       <ListThumbnail link={params.id} />
       {/* <ListSearch link={params.id} /> */}
-    </div>
+    </Suspense>
   );
 };
 
